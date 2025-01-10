3bdf81b83b5fa2e4fa0d0e104358dcf04e049042
ShubhamLEncureIT <shubham.loharkar@encureit.com>
2025-01-10 17:23:05 +0530

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
 
 export default function page() {
-  re  <h1>Login Page</h1>
+  return (
+    <div>
+      <h1>Login Page</h1>
       <Link href="/">Goto Home Page</Link>
     </div>
   )
