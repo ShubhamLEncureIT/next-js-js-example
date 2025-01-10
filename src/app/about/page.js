c88afbe013e182f20fba0bfab3abaa4b56ab30f4
ShubhamLEncureIT <shubham.loharkar@encureit.com>
2025-01-10 16:12:14 +0530

    updated

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,15 @@
+"use client";
 import Link from "next/link";
 
+import { useRouter } from "next/navigation";
+
 export default function page() {
+  const router = useRouter();
   return (
     <div>
-      <h1>About Page</h1>
-      <Link href="/">Goto Home Page</Link>
+      <h1>About Page</h1><br />
+      <Link href="/">Goto Home Page</Link> <br />
+      <button onClick={()=>router.push("/about/aboutStudent")}>About Student </button>
     </div>
   )
 }
