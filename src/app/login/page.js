0d4964920101ce91a3e2ecd58c36a041aa86228b
ShubhamLEncureIT <shubham.loharkar@encureit.com>
2025-01-10 17:46:58 +0530

    Changes in Page.tsx

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 export default function page() {
-  retudrn (
+  return (
     <div>
-      <h1>Login Page</h21>
+      <h1>Login Page</h1>
       <Link href="/">Goto Home Page</Link>
     </div>
   )
